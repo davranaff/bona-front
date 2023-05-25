5a894cb959781ed697f6aef81d2c6288d156b090
Sukhrob <95549412+davranaff@users.noreply.github.com>
2023-05-25 06:05:10 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
+        source: '/api/:path*',
         destination: 'https://bonafresco79.pythonanywhere.com/:path*',
       },
     ]
