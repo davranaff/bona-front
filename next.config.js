5c513b1078c53df8cf64bf7941a49f834dc3dc7a
Sukhrob <95549412+davranaff@users.noreply.github.com>
2023-05-25 05:56:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://bonafresco79.pythonanywhere.com/:path*',
+      },
+    ]
+  },
   experimental: {
     appDir: true,
   },
