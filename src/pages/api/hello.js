d2f0d57a434d62d322253aafdf5db7a931a39ad8
Sukhrob <95549412+davranaff@users.noreply.github.com>
2023-05-25 06:10:12 -0700

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from 'nextjs-cors';
+
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  await NextCors(req, res, {
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
 }
