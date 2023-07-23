c1384a21497f8613593945f16e17ced5f8837c61
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2023-07-23 17:03:07 +0500

    update pages/api/hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,8 +2,8 @@
 import NextCors from 'nextjs-cors';
 
 
-export default function handler(req, res) {
-  await NextCors(req, res, {
+export default async function handler(req, res) {
+   await NextCors(req, res, {
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       origin: '*',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
