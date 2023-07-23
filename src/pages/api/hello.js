9b502eaeb46df6836037b98de29da2af95b5d5db
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2023-07-23 17:05:13 +0500

    update pages/api/hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import NextCors from 'nextjs-cors';
+// import NextCors from 'nextjs-cors';
 
 
-export default async function handler(req, res) {
-   await NextCors(req, res, {
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: '*',
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-   });
-}
+// export default async function handler(req, res) {
+//    await NextCors(req, res, {
+//       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//       origin: '*',
+//       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//    });
+// }
