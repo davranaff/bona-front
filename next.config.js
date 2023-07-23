4478fcd2fdd4c63d77bf54f729d934cc456b5f58
Sukhrob <95549412+davranaff@users.noreply.github.com>
2023-07-23 17:48:19 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
