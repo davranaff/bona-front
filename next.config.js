e6b19fa80e985a7a4e3e4a9ab1786b032f658d6d
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2023-07-23 16:57:09 +0500

    add cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(cors({
+  origin: 'https://bonafresco79.pythonanywhere.com'
+}))
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+      ]
+      }
+    ]
+  },
   experimental: {
     appDir: true,
   },
