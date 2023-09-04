bd6e24101437e5c5de3effce82f48cd9852d33b1
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2023-09-04 13:31:44 +0500

    fixxx

--- src/app/services/api/consts.js
+++ src/app/services/api/consts.js
@@ -1,10 +1,3 @@
-import axios from "axios"
-
-axios.defaults.headers.common["Sec-Fetch-Mode"] = "cors"
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
-axios.defaults.headers.common["Access-Control-Allow-Methods"] = "GET,DELETE,PATCH,POST,PUT"
-axios.defaults.headers.common["Access-Control-Allow-Headers"] = "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-
 export const API_URL = "https://bonafresco79.pythonanywhere.com"
 export const API_URL_MEDIA = "https://bonafresco79.pythonanywhere.com/file"
 // export const API_URL = "http://localhost:8000"
